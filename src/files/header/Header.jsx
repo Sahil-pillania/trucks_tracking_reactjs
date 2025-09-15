338da36c4b605e161dda1af9892c19b1ed828eb0
Sahil pillania <sahilpillania88@gmail.com>
2025-09-15 21:25:07 +0530

    Added links and darkMode

--- src/files/header/Header.jsx
+++ src/files/header/Header.jsx
@@ -3,18 +3,30 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import DarkModeToggle from "../../Utilites/DarkMode";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary py-0">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src="favicon.jpg"
+            className="popping2 brand_logo"
+            alt="Brand_logo"
+          />
+          <span className="brand_text">Truck-Tracking</span>
+          {/* Truck-Tracking */}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -24,12 +36,12 @@ function Header() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link eventKey={2} href="#memes"></Nav.Link>
+            <Nav.Link href="#deets">
+              <DarkModeToggle />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
