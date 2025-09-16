c3b9f152f4fa7fbddaa67d311aa23f4574a8a479
Sahil pillania <sahilpillania88@gmail.com>
2025-09-16 22:50:44 +0530

    Updated router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./files/header/header";
 import Homepage from "./files/pages/homepage";
 import About from "./files/pages/about";
